19bcb05f6bdcff62845c60f0b109b4e93a98394a
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 23:43:30 +0530

    feat: integrate src/main.ts

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// Main entry point
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Router } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
